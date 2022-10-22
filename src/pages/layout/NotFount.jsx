d907ff6046058d515c7f61c73051664c17b8795c
Ezequiel <ezequielce490@gmail.com>
2022-10-22 17:01:28 -0300

    added routes, layout

---
+++ src/pages/layout/NotFount.jsx
@@ -0,0 +1,11 @@
+import errorIcon from '../../assets/image/404.png';
+
+const NotFound = () => {
+  return (
+    <>
+      <img src={errorIcon} alt="404 page" />
+    </>
+  );
+};
+
+export default NotFound;
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,1 +1,17 @@
 import { Navigate, useRoutes } from 'react-router-dom';
+import Dashboard from './dashboard/Dashboard';
+import DashboardLayout from './layout/DashboardLayoutt';
+import NotFound from './layout/NotFount';
+
+const Routes = () => {
+  return useRoutes([
+    {
+      path: '/',
+      element: <DashboardLayout />,
+      children: [{ path: '', element: <Dashboard /> }],
+    },
+    { path: '/404', element: <NotFound /> },
+    { path: '*', element: <Navigate to="/404" replace /> },
+  ]);
+};
+export default Routes;
