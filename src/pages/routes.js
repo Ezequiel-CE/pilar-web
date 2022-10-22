da28bfc1d9ad30ea82650cd3dc5a7841ec21ccbb
Ezequiel <ezequielce490@gmail.com>
2022-10-22 18:09:32 -0300

    added drawer, routes and pop menu

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,14 +1,20 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 import Dashboard from './dashboard/Dashboard';
+import FetchList from './fetchList/FetchList';
 import DashboardLayout from './layout/DashboardLayoutt';
 import NotFound from './layout/NotFount';
+import Todo from './todo/Todo';
 
 const Routes = () => {
   return useRoutes([
     {
       path: '/',
       element: <DashboardLayout />,
-      children: [{ path: '', element: <Dashboard /> }],
+      children: [
+        { path: '', element: <Dashboard /> },
+        { path: 'todo', element: <Todo /> },
+        { path: 'fetch-list', element: <FetchList /> },
+      ],
     },
     { path: '/404', element: <NotFound /> },
     { path: '*', element: <Navigate to="/404" replace /> },
